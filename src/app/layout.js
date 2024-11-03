ce7477406c0cef411b38f5d0c09ed77fd8c7ece9
comradecobweb <comradecobweb@gmail.com>
2024-11-03 21:01:09 +0100

    Added Wagmi configuration

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,10 +2,11 @@ import {Inter} from "next/font/google";
 import Footer from "@/components/layout/Footer";
 import "./globals.css";
 import {clsx} from "clsx";
-import {Web3Modal} from "@/app/context/web3modal";
 import {Toaster} from "@/components/ui/toaster";
 import Header from "@/components/layout/Header";
 import {ThemeProvider} from "@/app/context/theme-provider";
+import ContextProvider from "@/app/context/ContextProvider";
+import {headers} from "next/headers";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -16,25 +17,27 @@ export const metadata = {
 };
 
 export default function RootLayout({children}) {
+    const cookies = headers().get('cookie')
     return (
         <html lang="en" className={"h-svh w-svw min-h-full min-w-full p-5"}>
         <body className={clsx(inter.className, "flex flex-col w-full h-full overflow-hidden")}>
-        <Web3Modal>
-            <ThemeProvider
-                attribute="class"
-                defaultTheme="dark"
-                enableSystem
-                disableTransitionOnChange>
-                <Header className={"mt-2 mb-2"}/>
-                <div className={"self-stretch h-full flex justify-center overflow-y-auto mt-5 mb-5"}>
-                    <div className={"w-full h-full sm:w-5/6 lg:w-4/6"} id={'core'}>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange>
+
+            <Header className={"mt-2 mb-2"}/>
+            <div className={"self-stretch h-full flex justify-center overflow-y-auto mt-5 mb-5"}>
+                <div className={"w-full h-full sm:w-5/6 lg:w-4/6"} id={'core'}>
+                    <ContextProvider cookies={cookies}>
                         {children}
-                    </div>
+                    </ContextProvider>
                 </div>
-                <Footer/>
-                <Toaster/>
-            </ThemeProvider>
-        </Web3Modal>
+            </div>
+            <Footer/>
+            <Toaster/>
+        </ThemeProvider>
         </body>
         </html>
     );

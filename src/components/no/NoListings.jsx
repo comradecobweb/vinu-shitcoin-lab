f9407b3f128ff68b58fe64317cfd604d8686c315
comradecobweb <comradecobweb@gmail.com>
2024-12-08 21:21:51 +0100

    Added NoListings.jsx

---
+++ src/components/no/NoListings.jsx
@@ -0,0 +1,9 @@
+import Center from "@/components/Center";
+
+export default function NoListings() {
+    return (
+        <Center>
+            No listings have been created yet!
+        </Center>
+    );
+}

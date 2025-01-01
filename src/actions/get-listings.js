1a0e56cbe81bd21e813350c39fe89d32a3141201
comradecobweb <comradecobweb@gmail.com>
2025-01-01 11:01:16 +0100

    Fixed get-listings.js

--- src/actions/get-listings.js
+++ src/actions/get-listings.js
@@ -1,45 +1,35 @@
+'use server'
 import db from "@/lib/db";
 import {ethers} from "ethers";
 import rpcUrl from "@/lib/rpcUrl";
 
 export async function getAllListings() {
     let client;
-
     try {
         client = await db.connect();
     } catch (err) {
         return null;
     }
-
     let result;
     try {
-        result = await client.query({
-            text: 'SELECT * FROM listings;',
-        });
+        result = await client.query('SELECT * FROM listings;');
     } catch (err) {
         client.release();
         return null;
     }
-
     client.release();
 
     return result.rows;
 }
 
-
 export async function getUsersListings(address) {
     if (!address) return [];
-
     const listings = await getAllListings();
 
     return listings.filter(async (listing) => {
         try {
             const provider = new ethers.JsonRpcProvider(rpcUrl);
-
-            const abi = [
-                "function owner() public view returns (address)",
-            ];
-
+            const abi = ["function owner() public view returns (address)",];
             const contract = new ethers.Contract(address, abi, provider);
             const owner = await contract.owner();
 

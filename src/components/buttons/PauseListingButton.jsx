38e6740d6c79fe0e30e3d72a1b4d11c9c832fac4
comradecobweb <comradecobweb@gmail.com>
2025-01-18 21:41:49 +0100

    Added PauseListingButton.jsx

---
+++ src/components/buttons/PauseListingButton.jsx
@@ -0,0 +1,19 @@
+'use client'
+import {useContext} from "react";
+import {pausedListingContext} from "@/components/ManageListing"
+import {listingContext} from "@/components/Listing";
+import {Button} from "@/components/ui/button";
+import abi from "@/lib/listing-abi";
+import useWrite from "@/hooks/useWrite";
+
+export default function PauseListingButton() {
+    const [paused, setPaused] = useContext(pausedListingContext)
+    const listing = useContext(listingContext)
+    const {isFinished, write} = useWrite({
+                abi,
+                address: listing,
+                functionName: 'pause',
+            }, (e)=>{console.log(e)}, ()=>{setPaused(true)})
+
+    return <Button disabled={!isFinished} onClick={() => {write()}}>Pause</Button>
+}

5901ce236687aeba264a9dc5cb1adb3314d26ef3
comradecobweb <comradecobweb@gmail.com>
2025-01-18 21:42:30 +0100

    Added UnpauseListingButton.jsx

---
+++ src/components/buttons/UnpauseListingButton.jsx
@@ -0,0 +1,19 @@
+'use client'
+import {useContext} from "react";
+import {pausedListingContext} from "@/components/ManageListing"
+import {listingContext} from "@/components/Listing";
+import {Button} from "@/components/ui/button";
+import abi from "@/lib/listing-abi";
+import useWrite from "@/hooks/useWrite";
+
+export default function UnpauseListingButton(){
+    const [paused, setPaused] = useContext(pausedListingContext)
+    const listing = useContext(listingContext)
+    const {isFinished, write} = useWrite({
+                abi,
+                address: listing,
+                functionName: 'unpause',
+            }, (e)=>{console.log(e)}, ()=>{setPaused(false)})
+
+    return <Button disabled={!isFinished} onClick={() => {write()}}>Unpause</Button>
+}

3017c536ed3d6f638c64357a63df31215cdbd220
comradecobweb <comradecobweb@gmail.com>
2025-01-18 17:37:21 +0100

    Added /listings route

---
+++ src/app/listings/page.jsx
@@ -0,0 +1,11 @@
+'use client';
+import ListingsGrid from "@/components/ListingsGrid";
+
+export default function Page()
+{
+    return(
+        <div className={"w-full h-full"}>
+           <ListingsGrid/>
+        </div>
+    )
+}

7be6e74a7263a7082efc755adbc8e42b16f39e8c
comradecobweb <comradecobweb@gmail.com>
2025-01-21 18:13:23 +0100

    Improved listing manage grid

--- src/components/ManageListing.jsx
+++ src/components/ManageListing.jsx
@@ -4,7 +4,10 @@ import {PausedListingContextProvider} from "@/context/PausedListingContext";
 
 export default function ManageListing() {
 
-    return <div className={""}>
+    return <div className={"w-full h-full grid" +
+        " grid-cols-1 gap-y-5 overflow-y-auto" +
+        " sm:grid-cols-2 sm:grid-rows-3 sm:gap-4 sm:content-start" +
+        " md:grid-cols-3 md:gap-6  md:content-start md:items-start"}>
         <PausedListingContextProvider>
             <Pause/>
         </PausedListingContextProvider>
--- src/components/manage-listing/Pause.jsx
+++ src/components/manage-listing/Pause.jsx
@@ -3,13 +3,21 @@ import {useContext} from "react";
 import {pausedListingContext} from "@/context/PausedListingContext"
 import UnpauseListingButton from "@/components/buttons/UnpauseListingButton";
 import PauseListingButton from "@/components/buttons/PauseListingButton";
+import {Label} from "@/components/ui/label";
 
 export default function Pause() {
     const [paused] = useContext(pausedListingContext)
 
     return (
-        <>
+        <div className={"border-2 p-3 rounded-2xl flex flex-col size-full justify-around items-center"}>
+            <Label
+                className={"text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"}>
+                Pause
+            </Label>
+            <p className={"text-center text-sm text-muted-foreground"}>
+                Allows you to stop/resume all interactions with the listing.
+            </p>
             {paused ? <UnpauseListingButton/> : <PauseListingButton/>}
-        </>
+        </div>
     )
 }

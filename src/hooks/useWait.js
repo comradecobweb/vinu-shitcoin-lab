bb52cd2c2810bfb6511bfe2f3b1ec41919f3cfd8
comradecobweb <comradecobweb@gmail.com>
2025-01-28 16:25:42 +0100

    Added support for async functions in useWait.js

--- src/hooks/useWait.js
+++ src/hooks/useWait.js
@@ -1,16 +1,22 @@
 'use client'
 import {useEthersProvider} from "@/hooks/useEthers";
+import {toast} from "@/components/ui/use-toast";
 
-export default function useWait(onSuccess, onError = () => {}) {
+export default function useWait(onSuccess) {
     const provider = useEthersProvider();
+    const isAsync = onSuccess.constructor.name === "AsyncFunction";
 
     async function wait(hash) {
         try {
             const transaction = await provider.getTransaction(hash);
             await transaction.wait()
-            await onSuccess()
+            if (isAsync) await onSuccess()
+            else onSuccess()
         } catch (e) {
-            onError(e)
+            toast({
+                title: "Unexpected error!",
+                description: "Something went wrong, but we don't know what.",
+            })
         }
     }
 
